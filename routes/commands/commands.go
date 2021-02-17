4d7500579b1e0a6f0d9d1784fd035c4a3ca61bd6
Seraphim R.P <srp@kerygma.digital>
2021-02-17 14:06:05 -0500

    Use go:embeds on command route.

--- routes/commands/commands.go
+++ routes/commands/commands.go
@@ -4,13 +4,18 @@ package commands
 // Possibly create a Command struct to expect similar results? Might be easier said than done.
 
 import (
+	_ "embed" // for go:embeds
 	"encoding/json"
-	"io/ioutil"
-	"os"
 
 	"github.com/gofiber/fiber/v2"
-	"internal.kerygma.digital/kerygma-digital/biblebot/backend/utils/logger"
 	"internal.kerygma.digital/kerygma-digital/biblebot/backend/utils/slices"
+)
+
+var (
+	//go:embed data/command_list.json
+	commandListFile []byte
+
+	commandList = make(map[string][]string)
 )
 
 // RegisterRouter registers routers related to command processing.
@@ -25,20 +30,9 @@ func commandHandler(c *fiber.Ctx) error {
 }
 
 func isValidCommand(cm string) bool {
-	commandList := make(map[string][]string)
-
-	// Check if command is valid
-	// If we're testing, the working directory is tests/, so paths need to be adjusted for that.
-	dir := "./"
-	if _, err := os.Stat(dir + "data/names/command_list.json"); os.IsNotExist(err) {
-		dir = "./../"
+	if len(commandList) == 0 {
+		json.Unmarshal(commandListFile, &commandList)
 	}
-
-	file, err := ioutil.ReadFile(dir + "data/names/command_list.json")
-	if err != nil {
-		panic(logger.LogWithError("commands", "could not read command_list.json", nil))
-	}
-	json.Unmarshal(file, &commandList)
 
 	return slices.StringInSlice(cm, commandList["commands"]) || slices.StringInSlice(cm, commandList["owner_commands"])
 }
