3700c15f7cfdb68aeae94efe383218913cdf17a5
rocketFuel <alu@kerygma.digital>
2021-02-21 23:18:05 +1000

    Added Help command category

--- models/command.go
+++ models/command.go
@@ -1,10 +1,20 @@
 package models
 
-// Command is a basic command struct containing expected values.
+// Command is an umbrella struct that can be used to hold info & process 1 or more commands
 type Command struct {
-	Command        string
+	Command        []string
 	Params         []string
 	IsOwnerCommand bool
 
 	Process func() error
+}
+
+// Basic help command (biblebot)
+var Help = Command{
+	Command:        []string{"biblebot"},
+	Params:         nil,
+	IsOwnerCommand: false,
+	Process: func() error {
+		return nil
+	},
 }
