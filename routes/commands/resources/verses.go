95831341074dc4301dfb79cb1a632ae24fae7f1d
rocketFuel <alu@kerygma.digital>
2021-02-22 23:40:11 +1000

    Moved verses category to verses.go

--- routes/commands/resources/verses.go
+++ routes/commands/resources/verses.go
@@ -1,7 +1,22 @@
 package resources
 
-// This will handle random verses and searching for verses by keyword.
+import "internal.kerygma.digital/kerygma-digital/biblebot/backend/models"
 
+// Command struct for verse lookup commands.
+var verseFetchCommand = models.Command{
+	Command: []string{
+		"search",
+		"random",
+		"truerandom",
+	},
+	Params:         nil, // Valid reference if applicable
+	IsOwnerCommand: false,
+	Process: func() error {
+		return nil // To implement
+	},
+}
+
+// Handles verse lookups and random/truerandom verses.
 func verses() {
 	return
 }
