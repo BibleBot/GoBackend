c56a357ec1de287b6e3db6caa828dd7cd277ce7d
rocketFuel <alu@kerygma.digital>
2021-02-22 23:42:16 +1000

    Separated version and moved to versions.go

--- routes/commands/settings/versions.go
+++ routes/commands/settings/versions.go
@@ -1,7 +1,20 @@
 package settings
 
-// This will handle all version-related commands.
+import "internal.kerygma.digital/kerygma-digital/biblebot/backend/models"
 
+// Command struct for changing versions.
+var versionCommand = models.Command{
+	Command: []string{
+		"version",
+	},
+	Params:         nil, // Valid version or language using go:embed? or initialize or smth
+	IsOwnerCommand: false,
+	Process: func() error {
+		return nil // To implement
+	},
+}
+
+// Handles all version-related commands.
 func versions() {
 	return
 }
