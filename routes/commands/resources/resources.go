ed070903139b7bcb609656da401ca7d35f75c70f
rocketFuel <alu@kerygma.digital>
2021-02-22 23:39:47 +1000

    Moved creeds category into resources.go

--- routes/commands/resources/resources.go
+++ routes/commands/resources/resources.go
@@ -1,7 +1,24 @@
 package resources
 
-// This will handle creeds and catechisms.
+import "internal.kerygma.digital/kerygma-digital/biblebot/backend/models"
 
+// Command struct for any creed fetching commands
+var creedsCommand = models.Command{
+	Command: []string{
+		"creeds",
+		"apostles",
+		"nicene325",
+		"nicene",
+		"chalcedon",
+	},
+	Params:         nil,
+	IsOwnerCommand: false,
+	Process: func() error {
+		return nil // To implement
+	},
+}
+
+// Handles commands for the creeds and catechisms.
 func router() {
 	return
 }
