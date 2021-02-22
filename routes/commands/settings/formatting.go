608daa3afef75c078a978da5e4e0265042172618
rocketFuel <alu@kerygma.digital>
2021-02-22 23:40:52 +1000

    Moved formatting category to formatting.go

--- routes/commands/settings/formatting.go
+++ routes/commands/settings/formatting.go
@@ -1,7 +1,25 @@
 package settings
 
-// Handler for all formatting-related commands - verse numbers, headings, and display styles.
+import "internal.kerygma.digital/kerygma-digital/biblebot/backend/models"
 
+// Command stuct for the formatting command.
+var formattingCommand = models.Command{
+	Command: []string{
+		"formatting",
+	},
+	Params: []string{
+		"default",
+		"embed",
+		"blockquote",
+		"code",
+	},
+	IsOwnerCommand: false,
+	Process: func() error {
+		return nil // To implement
+	},
+}
+
+// Handles all formatting-related commands - verse numbers, headings, and display styles.
 func formatting() {
 	return
 }
