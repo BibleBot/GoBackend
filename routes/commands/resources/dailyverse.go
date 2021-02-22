9ac20fe344a53d9f145a5e2b87944fec4ccf4d56
rocketFuel <alu@kerygma.digital>
2021-02-22 23:39:16 +1000

    Separated dailyverse and moved into dailyverse.go

--- routes/commands/resources/dailyverse.go
+++ routes/commands/resources/dailyverse.go
@@ -1,7 +1,20 @@
 package resources
 
-// This will handle the verse of the day and automation related to it.
+import "internal.kerygma.digital/kerygma-digital/biblebot/backend/models"
 
+// Command struct for the dailyverse command.
+var dailyVerseCommand = models.Command{
+	Command: []string{
+		"dailyverse",
+	},
+	Params:         nil,
+	IsOwnerCommand: false,
+	Process: func() error {
+		return nil // To implement
+	},
+}
+
+// Handles the daily verse command and anything related to it.
 func dailyVerse() {
 	return
 }
