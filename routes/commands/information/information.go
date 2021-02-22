edee7b02150b166a176da649d85f9c5b17d535ff
rocketFuel <alu@kerygma.digital>
2021-02-22 23:37:56 +1000

    Moved help category to information.go

--- routes/commands/information/information.go
+++ routes/commands/information/information.go
@@ -1,5 +1,20 @@
 package information
 
+import "internal.kerygma.digital/kerygma-digital/biblebot/backend/models"
+
+// Command struct for help commands.
+var helpCommand = models.Command{
+	Command: []string{
+		"biblebot",
+	},
+	Params:         nil,
+	IsOwnerCommand: false,
+	Process: func() error {
+		return nil // To implement
+	},
+}
+
+// Handles help commands.
 func router() {
 	return
 }
