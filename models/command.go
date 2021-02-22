cb2b8bbc277efc33c99ba645bbfc327ded5c231c
rocketFuel <alu@kerygma.digital>
2021-02-22 23:43:22 +1000

    Updated command.go with applicable changes Marked remaining 2 categories as homeless (?)

--- models/command.go
+++ models/command.go
@@ -9,79 +9,7 @@ type Command struct {
 	Process func() error
 }
 
-// Basic help command (biblebot)
-var Help = Command{
-	Command: []string{
-		"biblebot",
-	},
-	Params:         nil,
-	IsOwnerCommand: false,
-	Process: func() error {
-		return nil // To implement
-	},
-}
-
-// Commands for changing formatting
-var Formatting = Command{
-	Command: []string{
-		"formatting",
-	},
-	Params: []string{
-		"default",
-		"embed",
-		"blockquote",
-		"code",
-	},
-	IsOwnerCommand: false,
-	Process: func() error {
-		return nil // To implement
-	},
-}
-
-// Commands for changing/fetching versions/language
-var Translations = Command{
-	Command: []string{
-		"version",
-		"language",
-	},
-	Params:         nil, // Valid version or language using go:embed? or initialize or smth
-	IsOwnerCommand: false,
-	Process: func() error {
-		return nil // To implement
-	},
-}
-
-// Commands for fetching verses
-var Verses = Command{
-	Command: []string{
-		"search",
-		"random",
-		"truerandom",
-		"dailyverse",
-	},
-	Params:         nil, // Valid reference if applicable
-	IsOwnerCommand: false,
-	Process: func() error {
-		return nil // To implement
-	},
-}
-
-// Commands for fetching creeds
-var Creeds = Command{
-	Command: []string{
-		"creeds",
-		"apostles",
-		"nicene325",
-		"nicene",
-		"chalcedon",
-	},
-	Params:         nil,
-	IsOwnerCommand: false,
-	Process: func() error {
-		return nil // To implement
-	},
-}
-
+/* 2 categories homeless at the moment
 // Commands for fetching miscellaneous info
 var MiscFetch = Command{
 	Command: []string{
@@ -109,4 +37,4 @@ var Owner = Command{
 	Process: func() error {
 		return nil // To implement
 	},
-}
+}*/
