e63a2a1a40bdf276cd6893524ba6e09b5522c087
rocketFuel <alu@kerygma.digital>
2021-02-22 23:41:57 +1000

    Separated languages and moved to languages.go

--- routes/commands/settings/languages.go
+++ routes/commands/settings/languages.go
@@ -1,8 +1,20 @@
 package settings
 
-// Handler for all language-related commands.
-// This should be quite similar to versions.go as they basically do the same thing, give or take a few features.
+import "internal.kerygma.digital/kerygma-digital/biblebot/backend/models"
 
+// Command struct for changing languages.
+var languageCommand = models.Command{
+	Command: []string{
+		"language",
+	},
+	Params:         nil, // Valid version or language using go:embed? or initialize or smth
+	IsOwnerCommand: false,
+	Process: func() error {
+		return nil // To implement
+	},
+}
+
+// Handles all language-related commands. (Should be quite similar to versions.go as they basically do the same thing, give or take some)
 func languages() {
 	return
 }
