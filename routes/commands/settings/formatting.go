f74dd0f73d923f84fcfc100f0fc81c24840fd754
rocketFuel <alu@kerygma.digital>
2021-03-10 08:29:59 +1000

    Started work on default, adjusted filter [ci skip]

--- routes/commands/settings/formatting.go
+++ routes/commands/settings/formatting.go
@@ -4,32 +4,27 @@ import (
 	"sync"
 
 	"internal.kerygma.digital/kerygma-digital/biblebot/backend/models"
-	// "internal.kerygma.digital/kerygma-digital/biblebot/backend/utils/slices"
+	"internal.kerygma.digital/kerygma-digital/biblebot/backend/utils/slices"
 )
 
 // FormatCommandRouter is a basic struct with functions to handle format-related commands.
 type FormatCommandRouter struct {
-	Commands []models.Command
+	DefaultCommand models.Command
+	Commands       []models.Command
 }
 
 var (
 	// formattingInstance is the singleton router used to process its respective commands
 	formattingInstance *FormatCommandRouter
 	formattingOnce     sync.Once
-
-	formattingCommand = models.Command{
-		Command: "formatting",
-		Process: func(params []string, ctx *models.Context) *models.CommandResponse {
-			return nil
-		},
-	}
 )
 
 // NewFormattingCommandRouter creates a FormatCommandRouter if one does not already exist
 func NewFormattingCommandRouter() *FormatCommandRouter {
 	formattingOnce.Do(func() {
 		formattingInstance = &FormatCommandRouter{
-			Commands: []models.Command{formattingCommand},
+			DefaultCommand: fmtDefault,
+			Commands:       []models.Command{},
 		}
 	})
 
@@ -38,33 +33,44 @@ func NewFormattingCommandRouter() *FormatCommandRouter {
 
 // Process checks which command process to run given the inputed command & parameters
 func (cr *FormatCommandRouter) Process(params []string, ctx *models.Context) *models.CommandResponse {
-	/* Not required since formatting only handles one command (?)
-	cm, ok := slices.FilterInterface(cr.Commands, func(cm interface{}) bool {
-		cmd, ok := cm.(models.Command)
-		return (params[0] == cmd.Command) && ok
+	cmMatches := slices.FilterInterface(cr.Commands, func(cm interface{}) bool {
+		if len(params) > 0 {
+			cmd, ok := cm.(models.Command)
+			return (params[0] == cmd.Command) && ok
+		}
+
+		return false
 	}).([]models.Command)
 
-	if ok {
-		// Strip first element of slice (is the command itself)
-		return cm.Process(params[1:], ctx)
-	}*/
-	if cr.Commands[0].Command == params[0] {
-		return cr.Commands[0].Process(params[1:], ctx)
+	var cm models.Command
+
+	if len(cmMatches) == 0 {
+		cm = cr.DefaultCommand
+	} else {
+		cm = cmMatches[0]
 	}
 
-	return nil
+	if len(params) == 0 {
+		return cm.Process([]string{}, ctx)
+	}
+
+	return cm.Process(params[1:], ctx)
 }
 
-// Set guild prefix
-
-// Set guild brackets (<>, [], {}, ()) <- allow more than one?
+// Get user preferences (headings, verse numbers, display style) and a list of formatting commands
+var fmtDefault = models.Command{
+	Command: "formatting",
+	Process: func(_ []string, ctx *models.Context) *models.CommandResponse {
+		return nil
+	},
+}
 
 // Get guild preferences (prefix, brackets)
+
+// Set guild brackets (<>, [], {}, ()) <- allow more than one?
 
 // Set user headings (true, false)
 
 // Set user verse numbers (true, false)
 
 // Set user display style (default, embed, blockquote, code)
-
-// Get user preferences (headings, verse numbers, display style)
