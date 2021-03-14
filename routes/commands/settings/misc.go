ab33d0b0936e2994ffa9f12a7b3bf74cfd7b3f56
Seraphim R.P <srp@kerygma.digital>
2021-03-13 23:18:27 -0500

    Get rid of misc.go, as its no longer going to be used. [ci skip]

--- routes/commands/settings/misc.go
+++
@@ -1,8 +0,0 @@
-package settings
-
-// This will handle miscellaneous settings, particularly server brackets and server command prefix.
-// Probably going to be grouped in formatting and deleted or used for other commands
-
-func misc() {
-	return
-}
